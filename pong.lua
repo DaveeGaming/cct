69cb1c91cb044674f970c4ef7e07dafc9e59cc2b
epimoni <45078821+DaveeGaming@users.noreply.github.com>
2024-01-04 11:57:47 +0100

    1

--- pong.lua
+++ pong.lua
@@ -1,17 +1,16 @@
----@diagnostic disable: undefined-global
 
 local width, height = term.getSize()
 
-local paddleheight = 4
+local paddleheight = 6
 
 local paddle1 = {
     x = 1,
-    y = height / 2 - paddlesize / 2,
+    y = height / 2 - paddleheight / 2,
 }
 
 local paddle2 = {
     x = width - 1,
-    y = height / 2 - paddlesize / 2,
+    y = height / 2 - paddleheight / 2,
 }
 
 local bullet = {
@@ -23,13 +22,24 @@ local bullet = {
 
 
 function Draw()
-    paintutils.drawFilledBox(paddle1.x,paddle1.y,paddle1.x, paddleheight)
-    paintutils.drawFilledBox(paddle2.x,paddle2.y,paddle2.x, paddleheight)
+    paintutils.drawFilledBox(paddle1.x,paddle1.y,paddle1.x, paddleheight, colors.white)
+    paintutils.drawFilledBox(paddle2.x,paddle2.y,paddle2.x, paddleheight, colors.white)
 end
 
 function Update()
+    local event, key, is_held = os.pullEvent("key")
+    if key == keys.w then
+        paddle1.x = paddle1.x + 1
+    elseif key == keys.s then
+        paddle1.x = paddle1.x - 1
+    end
 
 
+    if key == keys.up then
+        paddle1.x = paddle1.x + 1
+    elseif key == keys.down then
+        paddle1.x = paddle1.x - 1
+    end
 
 end
 
